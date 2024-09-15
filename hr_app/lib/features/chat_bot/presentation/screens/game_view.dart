8d02cb5a40a093c09600a81faab90c749c4ba3a7
Sandun Induranga <sandutharu40@gmail.com>
2024-09-15 19:36:47 +0530

    Refactor microtask scheduling and add GameView screen
    - Refactored microtask scheduling logic in `schedule_microtask.dart` for better optimization and error handling.
    - Added `GameView` screen in `lib/features/chat_bot/presentation/screens/game_view.dart` to display web content using `InAppWebView`.

--- hr_app/lib/features/chat_bot/presentation/screens/game_view.dart
+++ hr_app/lib/features/chat_bot/presentation/screens/game_view.dart
@@ -1,57 +1,200 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../../core/constants/color_codes.dart';
-import '../../../../core/widgets/common_page_widgets/common_page_boiler_plate.dart';
-import '../../../../core/widgets/gap_widgets/vertical_gap_consistent.dart';
-
-class GameView extends StatelessWidget {
+class GameView extends StatefulWidget {
   const GameView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarIconBrightness: Brightness.dark,
-        statusBarBrightness: Brightness.light,
-      ),
-    );
-    return CommonPageBoilerPlate(
-
-      horizontalPadding: 0,
-      pageBody: Container(
-        color: ColorCodes.whiteColor,
-        child: Column(
-          children: [
-            VerticalGapWidget(8.h),
-            Expanded(
-              child: InAppWebView(
-                // https://www.jopi.com/gam/key-quest/
-                // https://www.jopi.com/gam/painting-tiles/
-                // https://www.jopi.com/gam/space-slide/
-                // https://www.jopi.com/gam/master-sudoku/
-                initialUrlRequest: URLRequest(url: WebUri('https://www.jopi.com/gam/summer-maze/')),
-                initialOptions: InAppWebViewGroupOptions(
-                  crossPlatform: InAppWebViewOptions(
-                    useShouldOverrideUrlLoading: false,
-                    useOnLoadResource: false,
-                  ),
-                ),
-                onLoadStop:
-                    (InAppWebViewController controller, Uri? url) async {},
-                shouldOverrideUrlLoading: (controller, navigationAction) async {
-                  return NavigationActionPolicy.ALLOW;
-                },
-                onUpdateVisitedHistory: (controller, url, isReload) {},
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  State<GameView> createState() => _GameViewState();
 }
 
+class _GameViewState extends State<GameView> {
+  final GlobalKey webViewKey = GlobalKey();
+
+  InAppWebViewController? webViewController;
+  InAppWebViewSettings settings = InAppWebViewSettings(
+      isInspectable: kDebugMode,
+      mediaPlaybackRequiresUserGesture: false,
+      allowsInlineMediaPlayback: true,
+      iframeAllow: "camera; microphone",
+      iframeAllowFullscreen: true);
+
+  PullToRefreshController? pullToRefreshController;
+
+  late ContextMenu contextMenu;
+  String url = "";
+  double progress = 0;
+  final urlController = TextEditingController();
+
+  get webViewEnvironment => null;
+
+  @override
+  void initState() {
+    super.initState();
+
+    contextMenu = ContextMenu(
+        menuItems: [
+          ContextMenuItem(
+              id: 1,
+              title: "Special",
+              action: () async {
+                await webViewController?.clearFocus();
+              })
+        ],
+        settings: ContextMenuSettings(hideDefaultSystemContextMenuItems: false),
+        onCreateContextMenu: (hitTestResult) async {
+        },
+        onHideContextMenu: () {
+        },
+        onContextMenuActionItemClicked: (contextMenuItemClicked) async {
+          var id = contextMenuItemClicked.id;
+        });
+
+    pullToRefreshController = kIsWeb ||
+            ![TargetPlatform.iOS, TargetPlatform.android]
+                .contains(defaultTargetPlatform)
+        ? null
+        : PullToRefreshController(
+            settings: PullToRefreshSettings(
+              color: Colors.blue,
+            ),
+            onRefresh: () async {
+              if (defaultTargetPlatform == TargetPlatform.android) {
+                webViewController?.reload();
+              } else if (defaultTargetPlatform == TargetPlatform.iOS ||
+                  defaultTargetPlatform == TargetPlatform.macOS) {
+                webViewController?.loadUrl(
+                    urlRequest:
+                        URLRequest(url: await webViewController?.getUrl()));
+              }
+            },
+          );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Games")),
+        body: SafeArea(
+            child: Column(children: <Widget>[
+          Expanded(
+            child: Stack(
+              children: [
+                InAppWebView(
+                  key: webViewKey,
+                  initialUrlRequest: URLRequest(
+                      url: WebUri('https://www.jopi.com/gam/summer-maze/')),
+                  initialSettings: settings,
+                  contextMenu: contextMenu,
+                  pullToRefreshController: pullToRefreshController,
+                  onWebViewCreated: (controller) async {
+                    webViewController = controller;
+                  },
+                  onLoadStart: (controller, url) async {
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onPermissionRequest: (controller, request) async {
+                    return PermissionResponse(
+                        resources: request.resources,
+                        action: PermissionResponseAction.GRANT);
+                  },
+                  shouldOverrideUrlLoading:
+                      (controller, navigationAction) async {
+                    var uri = navigationAction.request.url!;
+
+                    if (![
+                      "http",
+                      "https",
+                      "file",
+                      "chrome",
+                      "data",
+                      "javascript",
+                      "about"
+                    ].contains(uri.scheme)) {
+                      if (await canLaunchUrl(uri)) {
+                        // Launch the App
+                        await launchUrl(
+                          uri,
+                        );
+                        // and cancel the request
+                        return NavigationActionPolicy.CANCEL;
+                      }
+                    }
+
+                    return NavigationActionPolicy.ALLOW;
+                  },
+                  onLoadStop: (controller, url) async {
+                    pullToRefreshController?.endRefreshing();
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onReceivedError: (controller, request, error) {
+                    pullToRefreshController?.endRefreshing();
+                  },
+                  onProgressChanged: (controller, progress) {
+                    if (progress == 100) {
+                      pullToRefreshController?.endRefreshing();
+                    }
+                    setState(() {
+                      this.progress = progress / 100;
+                      urlController.text = this.url;
+                    });
+                  },
+                  onUpdateVisitedHistory: (controller, url, isReload) {
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onConsoleMessage: (controller, consoleMessage) {
+                  },
+                ),
+                progress < 1.0
+                    ? LinearProgressIndicator(value: progress)
+                    : Container(),
+              ],
+            ),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                child: Icon(Icons.arrow_back),
+                onPressed: () {
+                  webViewController?.goBack();
+                },
+              ),
+              ElevatedButton(
+                child: Icon(Icons.arrow_forward),
+                onPressed: () {
+                  webViewController?.goForward();
+                },
+              ),
+              ElevatedButton(
+                child: Icon(Icons.refresh),
+                onPressed: () {
+                  webViewController?.reload();
+                },
+              ),
+            ],
+          ),
+        ])));
+  }
+
+  canLaunchUrl(WebUri uri) {
+    return uri.scheme == "https";
+  }
+
+  launchUrl(WebUri uri) {}
+}
--- hr_app/lib/main.dart
+++ hr_app/lib/main.dart
@@ -1,6 +1,26 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:hr_app/main_app.dart';
 
-void main() {
-  runApp(const MainApp());
+// void main() {
+//   runApp(const MainApp());
+// }
+final localhostServer = InAppLocalhostServer(documentRoot: 'assets');
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Permission.camera.request();
+  // await Permission.microphone.request();
+  // await Permission.storage.request();
+
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
+  }
+
+  if (!kIsWeb) {
+    await localhostServer.start();
+  }
+
+  runApp(MainApp());
 }
